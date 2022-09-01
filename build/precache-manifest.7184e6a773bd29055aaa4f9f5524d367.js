c45db4491899947ae117b0f0f849e6902833f68b
Anup Poudel <anupleo5@gmail.com>
2022-09-01 20:35:30 +0545

    build

---
+++ build/precache-manifest.7184e6a773bd29055aaa4f9f5524d367.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1819e11c9ca9186a06231f9211b7e1a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "462d3197b817b3c9b25c",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "91bba6d40e260058dcbb",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "462d3197b817b3c9b25c",
+    "url": "/static/js/2.d13b42e9.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.d13b42e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91bba6d40e260058dcbb",
+    "url": "/static/js/main.291f3639.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  },
+  {
+    "revision": "8d67c436182ee0cc9d1d2743fc3fc005",
+    "url": "/static/media/mypic.8d67c436.jpg"
+  }
+]);
